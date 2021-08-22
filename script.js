82cafa568f2390efc43a97bd44ebe9a9142e619c
Loctarogar <igrsl1990@gmail.com>
2021-08-22 21:03:47 +0200

    play/pause work

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const video = document.querySelector("video");
+const play = document.querySelector(".fa-play");
+const pause = document.querySelector(".fa-pause");
+const stop = document.querySelector(".fa-stop");
+const fullscreenOn = document.querySelector(".fa-expand");
+const fullscreenOff = document.querySelector(".fa-compress");
 
-function videoPlay() {
-  video.play();
+const iconPause = "fa-pause";
+const iconPlay = "fa-play";
+const hiddenClass = "hidden";
+
+const video = document.querySelector("video");
+const duration = video.duration;
+
+play.addEventListener("click", videoPlay.bind(null, iconPlay));
+pause.addEventListener("click", videoPlay.bind(null, iconPause));
+
+function videoPlay(command) {
+  if (command == "fa-play") {
+    console.log("play");
+    video.play();
+  } else {
+    video.pause();
+  }
+
+  changeIcon(command);
 }
 
-// videoPlay();
+function changeIcon(icon) {
+  play.classList.toggle(hiddenClass);
+  pause.classList.toggle(hiddenClass);
+}
